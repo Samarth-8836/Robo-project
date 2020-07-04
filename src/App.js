b92ce08fe8b77750622b053f5ddf4794afa91a17
samarth patel <psamarth8836@gmail.com>
2020-07-04 03:17:10 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      monsters : []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(i => (<h1 key = {i.id}> {i.name},{i.username},{i.email}</h1>) )}
+      </div>
+    );
+  }
+}
+
+export default App;
